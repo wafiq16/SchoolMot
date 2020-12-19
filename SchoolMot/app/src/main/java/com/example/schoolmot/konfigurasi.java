475a6ee4d5579e57e5ef15bbd7328c842c4af5c7
wafiq16 <wafiqkamal16@gmail.com>
2020-12-19 20:52:28 +0700

    add java file handler and configurer

---
+++ SchoolMot/app/src/main/java/com/example/schoolmot/konfigurasi.java
@@ -0,0 +1,18 @@
+package com.example.schoolmot;
+
+public class konfigurasi {
+    public static final String URL_ADD = "http://192.168.43.231/rpl/simpan.php";
+    public static final String URL_GET = "http://192.168.43.231/rpl/tampil.php";
+
+    public static final String KEY_ID = "no";
+    public static final String KEY_NAMA = "nama";
+    public static final String KEY_NRP = "nrp";
+    public static final String KEY_IDALAT = "idAlat";
+
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_ID = "no";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_NRP = "nrp";
+    public static final String TAG_IDALAT = "idAlat";
+
+}
